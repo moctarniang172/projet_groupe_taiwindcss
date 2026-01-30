640a7a305838b6c3e2a678cef954766cebf7037d
moctar <niangmoctar172@gmail.com>
2026-01-30 21:50:18 +0000

    Un projet de Travail de groupe

---
+++ index.js
@@ -0,0 +1,15 @@
+ tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            clifford: '#da373d',
+            orange: '#ffbd39',
+            orange2: '#ff9000',
+            noire: '#222222',
+          },
+          backgroundImage: {
+            fond_header : "url('/banner/banner-bg.jpg')"
+          }
+        }
+      }
+    }
