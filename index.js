16f13690d2bb71c9ea0eaec58c14fb98280fe52b
lenadiedhiou2002-dev <lenadiedhiou2002@gmail.com>
2026-02-02 17:25:03 +0100

    projet terminer

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
         extend: {
           colors: {
             clifford: '#da373d',
-            orange: '#ffbd39',
+           orange : '#ffbd39',
             orange2: '#ff9000',
             noire: '#222222',
           },
